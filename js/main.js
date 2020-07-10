9db62f33fde14ad9fe3d541b2dec383089b98e1b
luluxia <306924625@qq.com>
2020-07-10 15:39:38 +0800

    设计改版

--- js/main.js
+++ js/main.js
@@ -36,6 +36,56 @@ let app = new Vue({
     direction: '',
   },
   methods: {
+    star() {
+      let num=1;
+      let c=document.querySelector("#star");
+      c.width=document.body.clientWidth;
+      c.height=document.body.clientHeight;
+      let width=new Array();
+      let width2=new Array();
+      let width3=new Array();
+      let width4=new Array();
+      let linenum;
+      if(c.width>c.height){
+        linenum=(c.width-200)/50;
+      }else{
+        linenum=(c.height-200)/50;
+      }
+      for(i=0;i<=linenum;i++){
+          width[i]=200+Math.random()*200//星轨起跑线
+          width2[i]=0.001+Math.random()*Math.PI/180;//星轨速率
+          width3[i]=Math.random()*5+2;//星轨宽度
+          width4[i]=0.2+50*Math.random()*Math.PI/180;//星轨长度
+      }
+      let star = e => {
+          num++;
+          if(num==3000){
+            c.style.opacity = 0;
+              setTimeout(() => {
+                c.style.opacity = 0.8;
+                  num=0;
+              },1000);
+          }
+          e.clearRect(0,0,c.width,c.height);
+          e.fillStyle="rgba(255,255,255,0)";
+          e.strokeStyle="#FFF";
+          e.fillRect(0,0,c.width,c.height);
+          for(i=0;i<=linenum;i++){
+              e.lineWidth=width3[i];
+              e.beginPath();
+              e.arc(100,100,200+50*i,width2[i]*num+width[i],width2[i]*num+width4[i]+width[i]);
+              e.stroke();
+          }
+      }
+      let create = () => {
+          var e=c.getContext("2d");
+          e.lineCap="round";
+          setInterval(() => {
+              star(e);
+          },20);
+      }
+      create();
+    },
     slider_down(event) {
       this.sliderMove = 1
       this.sliderChange = 0
@@ -85,5 +135,6 @@ let app = new Vue({
   },
   mounted() {
     this.middle = document.body.clientWidth / 2
+    this.star()
   }
 })
